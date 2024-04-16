6755946dfaf3a12d7f6b2a73f688b8da61dd38a6
ZeRg0912 <serg.021@yandex.ru>
2024-04-16 16:02:45 +0300

    fix

--- 04/01/main.cpp
+++ 04/01/main.cpp
@@ -5,9 +5,9 @@
 
 int main() {
     std::map<std::string, std::vector<int>> coord;
-    coord["x"] = std::vector<int> {2, 4, 6, 8, 10, 12, 14, 16};
-    coord["y"] = std::vector<int> {3, 5, 7, 9, 11, 13, 15, 17};
-    coord["z"] = std::vector<int> {18, 19, 20, 21, 22, 23, 24, 25};
+    coord["x"] = std::vector<int> {1, 4, 6, 9, 3, 12, 5, 12};
+    coord["y"] = std::vector<int> {9, 6, 1, 7, 2, 13, 12, 21};
+    coord["z"] = std::vector<int> {1, 11, 12, 21, 6, 8, 3, 5};
 
     std::cout << "Cube:\n";
     Shape cube(3, coord);
